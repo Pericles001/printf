b9061526c50970c3b9e368ebf564f9a0f0353ad1
pericles001 <periclesadjovi@outlook.fr>
2021-10-05 21:26:49 +0100

    Add flag modifiers

--- main.h
+++ main.h
@@ -7,5 +7,17 @@
 #include <limits.h>
 
 
+/*Flag modifier Macros*/
+#define PLUS 1
+#define SPACE 2
+#define HASH 4
+#define ZERO 8
+#define NEG 16
+#define PLUS_FLAG (flags & 1)
+#define SPACE_FLAG ((flags >> 1) & 1)
+#define HASH_FLAG ((flags >> 2) & 1)
+#define ZERO_FLAG ((flags >> 3) & 1)
+#define NEG_FLAG ((flags >> 4) & 1)
+
 
 #endif /* MAIN_H */
