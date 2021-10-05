9052d7b1d4474542fa85a650970de7812428db20
pericles001 <periclesadjovi@outlook.fr>
2021-10-05 12:56:01 +0100

    Add function description

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*_printf -  a function that produces output according to a format.
+*@format:is a character string. The format string is composed of 
+*zero or more directives. 
+*Return: the number of characters printed
+* (excluding the null byte used to end output to strings)
+*/
+
+int _printf(char *format, ...)
+{
+
+}
