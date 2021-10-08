2c1f46c65dd4d9ae5df6dd746bedd88de2613e5d
underscoDe <developer.awounou@gmail.com>
2021-10-08 12:35:50 +0100

    include header file and prototypes

--- base_converters.c
+++ base_converters.c
@@ -2,3 +2,13 @@
  * File: base_converters.c
  * Auth: Arsene Awounou
  */
+
+#include "printf.h"
+
+unsigned int convert_sbase(buffer_t *output, long int num, char *base,
+        unsigned char flags, int wid, int prec);
+
+unsigned int convert_ubase(buffer_t *output,
+        unsigned long int num, char *base,
+        unsigned char flags, int wid, int prec);
+
