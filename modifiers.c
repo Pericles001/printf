ebb72b578f3d0cc1d2dd7f18374fb997d5aa0885
pericles001 <periclesadjovi@outlook.fr>
2021-10-09 00:22:58 +0100

    Write print_string_width function

--- modifiers.c
+++ modifiers.c
@@ -35,3 +35,33 @@ unsigned int print_width(buffer_t *output, unsigned int printed,
 	return (ret);
 }
 
+
+/**
+ *print_string_width - stores leading spaces to a buffer
+ *	for a width modifier
+ *@output: A buffer_t struct containing a character array
+ *@flags: flag modifier
+ *@wid: A width modifier
+ *@prec: A precision modifier
+ *@size: The size of string
+ *
+ *
+ *Return: The number of bytes stored to the buffer
+ */
+
+
+unsigned int print_string_width(buffer_t *output,
+		unsigned char flags, int wid, int precn, int size)
+{
+	unsigned int ret = 0;
+	char width = ' ';
+
+	if (NEG_FLAG == 0)
+	{
+		wid -= (prec == -1) ? size : prec;
+		for (; wid > 0; wid--)
+			ret += _memcpy(output, &width, 1);
+	}
+
+	return (ret);
+}
