3d7e4b3e2ea0e2937dc492a3567e055049ce24ac
underscoDe <developer.awounou@gmail.com>
2021-10-08 18:06:40 +0100

    convert_sbase func body

--- base_converters.c
+++ base_converters.c
@@ -24,3 +24,37 @@ unsigned int convert_ubase(buffer_t *output,
  *
  * Return: The number of bytes stored to the buffer.
  */
+unsigned int convert_sbase(buffer_t *output, long int num, char *base,
+        unsigned char flags, int wid, int prec)
+{
+    int size;
+    char digit, pad = '0';
+    unsigned int ret = 1;
+
+    for (size = 0; *(base + size);)
+        size++;
+
+    if (num >= size || num <= -size)
+        ret += convert_sbase(output, num / size, base,
+                flags, wid - 1, prec - 1);
+
+    else
+    {
+        /* Handle precision */
+        for (; prec > 1; prec--, wid--)
+            ret += _memcpy(output, &pad, 1);
+
+        /* Handle width */
+        if (NEG_FLAG == 0)
+        {
+            pad = (ZERO_FLAG == 1) ? '0' : ' ';
+            for (; wid > 1; wid--)
+                ret += _memcpy(output, &pad, 1);
+        }
+    }
+
+    digit = base[(num < 0 ? -1 : 1) * (num % size)];
+    _memcpy(output, &digit, 1);
+
+    return (ret);
+}
