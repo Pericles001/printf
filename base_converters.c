37c31b27d03bb36846822437da85becac124dacc
underscoDe <developer.awounou@gmail.com>
2021-10-08 17:48:08 +0100

    description for convert_sbase func

--- base_converters.c
+++ base_converters.c
@@ -12,3 +12,15 @@ unsigned int convert_ubase(buffer_t *output,
         unsigned long int num, char *base,
         unsigned char flags, int wid, int prec);
 
+/**
+ * convert_sbase - Converts a signed long to an inputted base and stores
+ *                 the result to a buffer contained in a struct.
+ * @output: A buffer_t struct containing a character array.
+ * @num: A signed long to be converted.
+ * @base: A pointer to a string containing the base to convert to.
+ * @flags: Flag modifiers.
+ * @wid: A width modifier.
+ * @prec: A precision modifier.
+ *
+ * Return: The number of bytes stored to the buffer.
+ */
