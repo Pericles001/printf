dd2cdbd26857549eca6822c3eb06201ce84e0aed
pericles001 <periclesadjovi@outlook.fr>
2021-10-09 00:33:58 +0100

    Write print_neg_width function

--- modifiers.c
+++ modifiers.c
@@ -65,3 +65,31 @@ unsigned int print_string_width(buffer_t *output,
 
 	return (ret);
 }
+
+
+/**
+ *
+ *print_neg_width - Stores trailing spaces to a buffer for a '-' flag
+ *@output: A buffer_t struct containing a character array
+ *@printed: The current number of bytes already stored to output
+ *	for a given specifier
+ *@flags: Flag modifiers
+ *@wid: A width modifier
+ *
+ *Return: The number of bytes stored to the buffer
+ */
+
+unsigned int print_neg_width(buffer_t *output, unsigned int printed,
+		unsigned char flags, int wid)
+{
+	unsigned int ret = 0;
+	char width = ' ';
+
+	if (NEG_FLAG == 1)
+	{
+		for (wid -= printed; wid > 0; wid--)
+			ret += _memcpy(output, &width, 1);
+	}
+
+	return (ret);
+}
