56b635485c491d3b5e484b95e63b202f7b0509e8
pericles001 <periclesadjovi@outlook.fr>
2021-10-09 05:18:53 +0100

    Done with init_buffer function

--- memory_helpers.c
+++ memory_helpers.c
@@ -40,3 +40,32 @@ unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n)
 
 
 }
+
+
+/**
+ * free_buffer - initialize a variable of struct type buffer_t 
+ *
+ *
+ * Return: A pointer to the initialized buffer_t
+ */
+
+buffer_t *init_buffer(void)
+{
+	buffer_t *output;
+
+	output = malloc(sizeof(buffer_t));
+	if (output == NULL)
+		return (NULL);
+
+	output->buffer = malloc(sizeof(char) * 1024);
+	if (output->buffer == NULL)
+	{
+		free(output);
+		return (NULL);
+	}
+
+	output->start = output->buffer;
+	output->len = 0;
+
+	return (output);
+}
