6d967ada7947807948bb612f3c32dc334b93d7b1
pericles001 <periclesadjovi@outlook.fr>
2021-10-09 05:52:36 +0100

    Written function run_printf

--- _printf.c
+++ _printf.c
@@ -20,18 +20,53 @@ void cleanup(va_list args, buffer_t *output)
 
 
 
+/**
+ * run_printf - Reads through the format string for _printf
+ * @format: Character string to print - may contain directives
+ * @output: A buffer_t struct containing a buffer
+ * @args:A va_list of arguments
+ *
+ * Return: The number of characters stored to output
+ */
 
+int run_printf(const char *format, va_list args, buffer_t *output)
+{
 
+	int i, wid, prec, ret = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
 
-
-
-
-
-
-
-
-
-
+	for (i = 0; *(format + i); i++)
+	{
+		len = 0;
+		if (*(format + i) == '%')
+		{
+			tmp = 0;
+			flags = handle_flags(format + i + 1, &tmp);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1,
+					&tmp);
+			len = handle_specifiers(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(args, output, flags, wid, prec, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret = -1;
+				break;
+			}
+		}
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
+	cleanup(args, output);
+	return (ret);
+}
 
 
 
@@ -39,8 +74,8 @@ void cleanup(va_list args, buffer_t *output)
 
 /**
 *_printf -  a function that produces output according to a format.
-*@format:is a character string. The format string is composed of 
-*zero or more directives. 
+*@format:is a character string. The format string is composed of
+*zero or more directives.
 *Return: the number of characters printed
 * (excluding the null byte used to end output to strings)
 */
