d7aa6de5e5bea51189158da1cf9ec9811d36c73c
underscoDe <developer.awounou@gmail.com>
2021-10-09 02:40:50 +0100

    convert_ubase function body

--- base_converters.c
+++ base_converters.c
@@ -74,5 +74,38 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
         unsigned char flags, int wid, int prec)
 {
+    unsigned int size, ret = 1;
+    char digit, pad = '0', *lead = "0x";
 
+    for (size = 0; *(base + size);)
+        size++;
+
+    if (num >= size)
+        ret += convert_ubase(output, num / size, base,
+                flags, wid - 1, prec - 1);
+
+    else
+    {
+        if (((flags >> 5) & 1) == 1) /* Printing a ptr address */
+        {
+            wid -= 2;
+            prec -= 2;
+        }
+        for (; prec > 1; prec--, wid--) /* Handle precision */
+            ret += _memcpy(output, &pad, 1);
+
+        if (NEG_FLAG == 0) /* Handle width */
+        {
+            pad = (ZERO_FLAG == 1) ? '0' : ' ';
+            for (; wid > 1; wid--)
+                ret += _memcpy(output, &pad, 1);
+        }
+        if (((flags >> 5) & 1) == 1) /* Print 0x for ptr address */
+            ret += _memcpy(output, lead, 2);
+    }
+
+    digit = base[(num % size)];
+    _memcpy(output, &digit, 1);
+
+    return (ret);
 }
