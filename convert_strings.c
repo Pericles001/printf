49754fab56a0395f6d8555ab3d08ff3e29da1c76
underscoDe <developer.awounou@gmail.com>
2021-10-09 03:02:31 +0100

    convert_s func body

--- convert_strings.c
+++ convert_strings.c
@@ -1,13 +1,13 @@
 #include "printf.h"
 
 unsigned int convert_s(va_list args, buffer_t *output,
-        unsigned char flags, int wid, int prec, unsigned char len);
+		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_S(va_list args, buffer_t *output,
-        unsigned char flags, int wid, int prec, unsigned char len);
+		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_r(va_list args, buffer_t *output,
-        unsigned char flags, int wid, int prec, unsigned char len);
+		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_R(va_list args, buffer_t *output,
-        unsigned char flags, int wid, int prec, unsigned char len);
+		unsigned char flags, int wid, int prec, unsigned char len);
 
 /**
  * convert_s - Converts an argument to a string and
@@ -22,7 +22,33 @@ unsigned int convert_R(va_list args, buffer_t *output,
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int convert_s(va_list args, buffer_t *output,
-        unsigned char flags, int wid, int prec, unsigned char len)
+		unsigned char flags, int wid, int prec, unsigned char len)
 {
+	char *str, *null = "(null)";
+	int size;
+	unsigned int ret = 0;
 
+	(void)flags;
+	(void)len;
+
+	str = va_arg(args, char *);
+	if (str == NULL)
+		return (_memcpy(output, null, 6));
+
+	for (size = 0; *(str + size);)
+		size++;
+
+	ret += print_string_width(output, flags, wid, prec, size);
+
+	prec = (prec == -1) ? size : prec;
+	while (*str != '\0' && prec > 0)
+	{
+		ret += _memcpy(output, str, 1);
+		prec--;
+		str++;
+	}
+
+	ret += print_neg_width(output, ret, flags, wid);
+
+	return (ret);
 }
