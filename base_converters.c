b0713465b000418d8523fe2453f1028a97b32a64
underscoDe <developer.awounou@gmail.com>
2021-10-09 02:30:34 +0100

    short desc for convert_ubase

--- base_converters.c
+++ base_converters.c
@@ -58,3 +58,10 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 
     return (ret);
 }
+
+/**
+ * convert_ubase - Converts an unsigned long to an inputted base and
+ *                 stores the result to a buffer contained in a struct.
+ * @output: A buffer_t struct containing a character array.
+ * @num: An unsigned long to be converted.
+ * @base: A pointer to a string containing the base to convert to.
