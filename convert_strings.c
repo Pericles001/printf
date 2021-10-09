be4252393f3af5129ab75896777722e1801f7adb
underscoDe <developer.awounou@gmail.com>
2021-10-09 03:09:22 +0100

    convert_r body

--- convert_strings.c
+++ convert_strings.c
@@ -119,4 +119,32 @@ unsigned int convert_S(va_list args, buffer_t *output,
  */
 unsigned int convert_r(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len)
-{}
+{
+	char *str, *null = "(null)";
+	int size, end, i;
+	unsigned int ret = 0;
+
+	(void)flags;
+	(void)len;
+
+	str = va_arg(args, char *);
+	if (str == NULL)
+		return (_memcpy(output, null, 6));
+
+	for (size = 0; *(str + size);)
+		size++;
+
+	ret += print_string_width(output, flags, wid, prec, size);
+
+	end = size - 1;
+	prec = (prec == -1) ? size : prec;
+	for (i = 0; end >= 0 && i < prec; i++)
+	{
+		ret += _memcpy(output, (str + end), 1);
+		end--;
+	}
+
+	ret += print_neg_width(output, ret, flags, wid);
+
+	return (ret);
+}
