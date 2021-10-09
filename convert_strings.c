d141d12b6cb1cbf0dac78a262de6f13a898ddd00
underscoDe <developer.awounou@gmail.com>
2021-10-09 03:01:19 +0100

    convert_s func description

--- convert_strings.c
+++ convert_strings.c
@@ -9,3 +9,15 @@ unsigned int convert_r(va_list args, buffer_t *output,
 unsigned int convert_R(va_list args, buffer_t *output,
         unsigned char flags, int wid, int prec, unsigned char len);
 
+/**
+ * convert_s - Converts an argument to a string and
+ *             stores it to a buffer contained in a struct.
+ * @args: A va_list pointing to the argument to be converted.
+ * @flags: Flag modifiers.
+ * @wid: A width modifier.
+ * @prec: A precision modifier.
+ * @len: A length modifier.
+ * @output: A buffer_t struct containing a character array.
+ *
+ * Return: The number of bytes stored to the buffer.
+ */
