bda168d6fa1eda869488b677d8770abd8865c7c0
pericles001 <periclesadjovi@outlook.fr>
2021-10-09 05:58:49 +0100

    Describe function handle_flags

--- handlers.c
+++ handlers.c
@@ -7,3 +7,12 @@ int handle_width(va_list args, const char *modifier, char *index);
 int handle_precision(va_list args, const char *modifier, char *index);
 unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
 		unsigned char, int, int, unsigned char);
+
+/**
+* handle_flags - Matches flags with corresponding values.
+* @flag: A pointer to a potential string of flags.
+* @index: An index counter for the original format string.
+*
+* Return: If flag characters are matched - a corresponding value.
+*         Otherwise - 0.
+*/
