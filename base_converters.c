3b3294cc0420c0ac07a16dd33889b5dd1bf20b43
underscoDe <developer.awounou@gmail.com>
2021-10-09 04:50:31 +0100

    convert indentation to tabs

--- base_converters.c
+++ base_converters.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 unsigned int convert_sbase(buffer_t *output, long int num, char *base,
-        unsigned char flags, int wid, int prec);
+						   unsigned char flags, int wid, int prec);
 
 unsigned int convert_ubase(buffer_t *output,
-        unsigned long int num, char *base,
-        unsigned char flags, int wid, int prec);
+						   unsigned long int num, char *base,
+						   unsigned char flags, int wid, int prec);
 
 /**
  * convert_sbase - Converts a signed long to an inputted base and stores
@@ -20,38 +20,38 @@ unsigned int convert_ubase(buffer_t *output,
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int convert_sbase(buffer_t *output, long int num, char *base,
-        unsigned char flags, int wid, int prec)
+						   unsigned char flags, int wid, int prec)
 {
-    int size;
-    char digit, pad = '0';
-    unsigned int ret = 1;
+	int size;
+	char digit, pad = '0';
+	unsigned int ret = 1;
 
-    for (size = 0; *(base + size);)
-        size++;
+	for (size = 0; *(base + size);)
+		size++;
 
-    if (num >= size || num <= -size)
-        ret += convert_sbase(output, num / size, base,
-                flags, wid - 1, prec - 1);
+	if (num >= size || num <= -size)
+		ret += convert_sbase(output, num / size, base,
+							 flags, wid - 1, prec - 1);
 
-    else
-    {
-        /* Handle precision */
-        for (; prec > 1; prec--, wid--)
-            ret += _memcpy(output, &pad, 1);
+	else
+	{
+		/* Handle precision */
+		for (; prec > 1; prec--, wid--)
+			ret += _memcpy(output, &pad, 1);
 
-        /* Handle width */
-        if (NEG_FLAG == 0)
-        {
-            pad = (ZERO_FLAG == 1) ? '0' : ' ';
-            for (; wid > 1; wid--)
-                ret += _memcpy(output, &pad, 1);
-        }
-    }
+		/* Handle width */
+		if (NEG_FLAG == 0)
+		{
+			pad = (ZERO_FLAG == 1) ? '0' : ' ';
+			for (; wid > 1; wid--)
+				ret += _memcpy(output, &pad, 1);
+		}
+	}
 
-    digit = base[(num < 0 ? -1 : 1) * (num % size)];
-    _memcpy(output, &digit, 1);
+	digit = base[(num < 0 ? -1 : 1) * (num % size)];
+	_memcpy(output, &digit, 1);
 
-    return (ret);
+	return (ret);
 }
 
 /**
@@ -67,40 +67,40 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
-        unsigned char flags, int wid, int prec)
+						   unsigned char flags, int wid, int prec)
 {
-    unsigned int size, ret = 1;
-    char digit, pad = '0', *lead = "0x";
+	unsigned int size, ret = 1;
+	char digit, pad = '0', *lead = "0x";
 
-    for (size = 0; *(base + size);)
-        size++;
+	for (size = 0; *(base + size);)
+		size++;
 
-    if (num >= size)
-        ret += convert_ubase(output, num / size, base,
-                flags, wid - 1, prec - 1);
+	if (num >= size)
+		ret += convert_ubase(output, num / size, base,
+							 flags, wid - 1, prec - 1);
 
-    else
-    {
-        if (((flags >> 5) & 1) == 1) /* Printing a ptr address */
-        {
-            wid -= 2;
-            prec -= 2;
-        }
-        for (; prec > 1; prec--, wid--) /* Handle precision */
-            ret += _memcpy(output, &pad, 1);
+	else
+	{
+		if (((flags >> 5) & 1) == 1) /* Printing a ptr address */
+		{
+			wid -= 2;
+			prec -= 2;
+		}
+		for (; prec > 1; prec--, wid--) /* Handle precision */
+			ret += _memcpy(output, &pad, 1);
 
-        if (NEG_FLAG == 0) /* Handle width */
-        {
-            pad = (ZERO_FLAG == 1) ? '0' : ' ';
-            for (; wid > 1; wid--)
-                ret += _memcpy(output, &pad, 1);
-        }
-        if (((flags >> 5) & 1) == 1) /* Print 0x for ptr address */
-            ret += _memcpy(output, lead, 2);
-    }
+		if (NEG_FLAG == 0) /* Handle width */
+		{
+			pad = (ZERO_FLAG == 1) ? '0' : ' ';
+			for (; wid > 1; wid--)
+				ret += _memcpy(output, &pad, 1);
+		}
+		if (((flags >> 5) & 1) == 1) /* Print 0x for ptr address */
+			ret += _memcpy(output, lead, 2);
+	}
 
-    digit = base[(num % size)];
-    _memcpy(output, &digit, 1);
+	digit = base[(num % size)];
+	_memcpy(output, &digit, 1);
 
-    return (ret);
+	return (ret);
 }
--- convert_c_percent_p.c
+++ convert_c_percent_p.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 unsigned int convert_c(va_list args, buffer_t *output,
-        unsigned char flags, int wid, int prec, unsigned char len);
+		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_percent(va_list args, buffer_t *output,
-        unsigned char flags, int wid, int prec, unsigned char len);
+		unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_p(va_list args, buffer_t *output,
-        unsigned char flags, int wid, int prec, unsigned char len);
+		unsigned char flags, int wid, int prec, unsigned char len);
 
 /**
  * convert_c - Converts an argument to an unsigned char and
@@ -20,21 +20,21 @@ unsigned int convert_p(va_list args, buffer_t *output,
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int convert_c(va_list args, buffer_t *output,
-        unsigned char flags, int wid, int prec, unsigned char len)
+		unsigned char flags, int wid, int prec, unsigned char len)
 {
-    char c;
-    unsigned int ret = 0;
+	char c;
+	unsigned int ret = 0;
 
-    (void)prec;
-    (void)len;
+	(void)prec;
+	(void)len;
 
-    c = va_arg(args, int);
+	c = va_arg(args, int);
 
-    ret += print_width(output, ret, flags, wid);
-    ret += _memcpy(output, &c, 1);
-    ret += print_neg_width(output, ret, flags, wid);
+	ret += print_width(output, ret, flags, wid);
+	ret += _memcpy(output, &c, 1);
+	ret += print_neg_width(output, ret, flags, wid);
 
-    return (ret);
+	return (ret);
 }
 
 /**
@@ -50,20 +50,20 @@ unsigned int convert_c(va_list args, buffer_t *output,
  * Return: The number of bytes stored to the buffer (always 1).
  */
 unsigned int convert_percent(va_list args, buffer_t *output,
-        unsigned char flags, int wid, int prec, unsigned char len)
+		unsigned char flags, int wid, int prec, unsigned char len)
 {
-    char percent = '%';
-    unsigned int ret = 0;
+	char percent = '%';
+	unsigned int ret = 0;
 
-    (void)args;
-    (void)prec;
-    (void)len;
+	(void)args;
+	(void)prec;
+	(void)len;
 
-    ret += print_width(output, ret, flags, wid);
-    ret += _memcpy(output, &percent, 1);
-    ret += print_neg_width(output, ret, flags, wid);
+	ret += print_width(output, ret, flags, wid);
+	ret += _memcpy(output, &percent, 1);
+	ret += print_neg_width(output, ret, flags, wid);
 
-    return (ret);
+	return (ret);
 }
 
 /**
@@ -79,22 +79,22 @@ unsigned int convert_percent(va_list args, buffer_t *output,
  * Return: The number of bytes stored to the buffer.
  */
 unsigned int convert_p(va_list args, buffer_t *output,
-        unsigned char flags, int wid, int prec, unsigned char len)
+		unsigned char flags, int wid, int prec, unsigned char len)
 {
-    char *null = "(nil)";
-    unsigned long int address;
-    unsigned int ret = 0;
+	char *null = "(nil)";
+	unsigned long int address;
+	unsigned int ret = 0;
 
-    (void)len;
+	(void)len;
 
-    address = va_arg(args, unsigned long int);
-    if (address == '\0')
-        return (_memcpy(output, null, 5));
+	address = va_arg(args, unsigned long int);
+	if (address == '\0')
+		return (_memcpy(output, null, 5));
 
-    flags |= 32;
-    ret += convert_ubase(output, address, "0123456789abcdef",
-            flags, wid, prec);
-    ret += print_neg_width(output, ret, flags, wid);
+	flags |= 32;
+	ret += convert_ubase(output, address, "0123456789abcdef",
+			flags, wid, prec);
+	ret += print_neg_width(output, ret, flags, wid);
 
-    return (ret);
+	return (ret);
 }
