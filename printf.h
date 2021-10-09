60fef3e1fabe212d30dc7670c0555e32e7c59b39
underscoDe <developer.awounou@gmail.com>
2021-10-09 04:18:43 +0100

    fixed typo

--- printf.h
+++ printf.h
@@ -65,7 +65,7 @@ unsigned int convert_c(va_list args, buffer_t *output,
 					   unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_s(va_list args, buffer_t *output,
 					   unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int converter_di(va_list args, buffer_t *output,
+unsigned int convert_di(va_list args, buffer_t *output,
 						  unsigned char flags, int wid, int prec, unsigned char len);
 unsigned int convert_percent(va_list args, buffer_t *output,
 							 unsigned char flags, int wid, int prec, unsigned char len);
