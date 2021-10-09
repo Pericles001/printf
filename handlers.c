9fd20ec965f33e27dd60dacdbc6c89b72953e9af
pericles001 <periclesadjovi@outlook.fr>
2021-10-09 06:01:12 +0100

    Add function handle_flags

--- handlers.c
+++ handlers.c
@@ -16,3 +16,37 @@ unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
 * Return: If flag characters are matched - a corresponding value.
 *         Otherwise - 0.
 */
+
+unsigned char handle_flags(const char *flag, char *index)
+{
+int i, j;
+unsigned char ret = 0;
+flag_t flags[] = {
+{'+', PLUS},
+{' ', SPACE},
+{'#', HASH},
+{'0', ZERO},
+{'-', NEG},
+{0, 0}
+};
+
+for (i = 0; flag[i]; i++)
+{
+for (j = 0; flags[j].flag != 0; j++)
+{
+if (flag[i] == flags[j].flag)
+{
+(*index)++;
+if (ret == 0)
+ret = flags[j].value;
+else
+ret |= flags[j].value;
+break;
+}
+}
+if (flags[j].value == 0)
+break;
+}
+
+return (ret);
+}
