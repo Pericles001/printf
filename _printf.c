30f9f4817163e9548e36bc89ed73310105ff764e
underscoDe <developer.awounou@gmail.com>
2021-10-09 03:36:46 +0100

    _printf prototype

--- _printf.c
+++ _printf.c
@@ -2,13 +2,13 @@
 
 /**
 *_printf -  a function that produces output according to a format.
-*@format:is a character string. The format string is composed of 
-*zero or more directives. 
+*@format:is a character string. The format string is composed of
+*zero or more directives.
 *Return: the number of characters printed
 * (excluding the null byte used to end output to strings)
 */
 
-int _printf(char *format, ...)
+int _printf(const char *format, ...)
 {
 	buffer_t *output;
 	va_list args;
