7bb7eb241b645e00e2f396968e4c91e45e4cbb44
pericles001 <periclesadjovi@outlook.fr>
2021-10-09 06:08:31 +0100

    Written function handle_width

--- handlers.c
+++ handlers.c
@@ -88,3 +88,26 @@ unsigned char handle_length(const char *modifier, char *index)
  * Return: If a width modifier is matched - its value.
  *         Otherwise - 0.
  */
+int handle_width(va_list args, const char *modifier, char *index)
+{
+int value = 0;
+
+while ((*modifier >= '0' && *modifier <= '9') || (*modifier == '*'))
+{
+(*index)++;
+
+if (*modifier == '*')
+{
+value = va_arg(args, int);
+if (value <= 0)
+return (0);
+return (value);
+}
+
+value *= 10;
+value += (*modifier - '0');
+modifier++;
+}
+
+return (value);
+}
