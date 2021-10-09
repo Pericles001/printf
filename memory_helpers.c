13f5b135e78d1bf3d6f29178f6e8b773c5f54b44
pericles001 <periclesadjovi@outlook.fr>
2021-10-09 05:06:27 +0100

    Definition of functions

---
+++ memory_helpers.c
@@ -0,0 +1,29 @@
+#include "printf.h"
+
+
+
+
+
+
+
+
+
+
+unsigned int _memcpy();
+void free_buffer(buffer_t *output);
+buffer_t *init_buffer(void);
+
+
+/**
+ *_memcpy - copies n bytes from memory area src to
+ *	the buffer contained in a buffer_t struct
+ *@output: A buffer_t struct
+ *@src: A pointer to the memory area to copy
+ *@n: the number of bytes to be copied
+ *
+ *return: The number of bytes copied
+ */
+
+unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n)
+{
+}
