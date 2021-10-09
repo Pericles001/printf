8978293a8e14f5e6b72ea256ce0685047915bb99
pericles001 <periclesadjovi@outlook.fr>
2021-10-09 05:57:58 +0100

    Add functions definitions

--- handlers.c
+++ handlers.c
@@ -1,3 +1,9 @@
 #include "printf.h"
 
 
+unsigned char handle_flags(const char *flag, char *index);
+unsigned char handle_length(const char *modifier, char *index);
+int handle_width(va_list args, const char *modifier, char *index);
+int handle_precision(va_list args, const char *modifier, char *index);
+unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
+		unsigned char, int, int, unsigned char);
