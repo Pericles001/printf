68149dd36c48ef738c7a8f32cb41cf0048d4019f
pericles001 <periclesadjovi@outlook.fr>
2021-10-09 05:32:14 +0100

    Written cleanup function

--- _printf.c
+++ _printf.c
@@ -1,5 +1,42 @@
 #include "printf.h"
 
+
+void cleanup(va_list args, buffer_t *output);
+int run_printf(const char *format, va_list args, buffer_t *output);
+int _printf(const char *format, ...);
+
+/**
+ * cleanup - Performs cleanup for _printf
+ * @args: a va_list of arguments to _printf
+ *@output: A buffer_t struct
+ */
+
+void cleanup(va_list args, buffer_t *output)
+{
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
 *_printf -  a function that produces output according to a format.
 *@format:is a character string. The format string is composed of 
