89ddb20f11e2ce2979681f2f9f96a4717898d110
pericles001 <periclesadjovi@outlook.fr>
2021-10-09 05:27:19 +0100

    Write _printf function

--- _printf.c
+++ _printf.c
@@ -10,5 +10,19 @@
 
 int _printf(char *format, ...)
 {
+	buffer_t *output;
+	va_list args;
+	int ret;
 
+	if (format == NULL)
+		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	ret = run_printf(format, args, output);
+
+	return (ret);
 }
