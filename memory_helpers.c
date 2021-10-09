fa5a1473ed8b4040d870b69b5ed3c05cd16557f4
pericles001 <periclesadjovi@outlook.fr>
2021-10-09 05:20:41 +0100

    Done with free_buffer function

--- memory_helpers.c
+++ memory_helpers.c
@@ -43,6 +43,22 @@ unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n)
 
 
 /**
+ * free_buffer - Frees a buffer_t struct
+ * @output: The buffer_t struct to be freed
+ */
+
+void free_buffer(buffer_t *output)
+{
+	free(output->start);
+	free(output);
+}
+
+
+
+
+
+
+/**
  * free_buffer - initialize a variable of struct type buffer_t 
  *
  *
