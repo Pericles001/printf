b6011fac46fe732c3892c1277fc5d1b810b4ec56
pericles001 <periclesadjovi@outlook.fr>
2021-10-09 06:13:14 +0100

    Written function handle_specifiers

--- handlers.c
+++ handlers.c
@@ -173,3 +173,33 @@ return (value);
  * Return: If a conversion function is matched - a pointer to the function.
  *         Otherwise - NULL.
  */
+unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
+ 		unsigned char, int, int, unsigned char)
+{
+int i;
+converter_t converters[] = {
+{'c', convert_c},
+{'s', convert_s},
+{'d', convert_di},
+{'i', convert_di},
+{'%', convert_percent},
+{'b', convert_b},
+{'u', convert_u},
+{'o', convert_o},
+{'x', convert_x},
+{'X', convert_X},
+{'S', convert_S},
+{'p', convert_p},
+{'r', convert_r},
+{'R', convert_R},
+{0, NULL}
+};
+
+for (i = 0; converters[i].func; i++)
+{
+if (converters[i].specifier == *specifier)
+return (converters[i].func);
+}
+
+return (NULL);
+}
